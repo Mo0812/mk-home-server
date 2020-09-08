a69a33cfc5313ed8b9efea6755cef8f235fa0700
mkanzler <developer@moritzkanzler.de>
2020-09-08 21:59:19 +0200

    add Tradfri and Monitoring

---
+++ src/controller/Tradfri/Tradfri.js
@@ -0,0 +1,55 @@
+const events = require("events");
+const tradfriLib = require("node-tradfri-client");
+const TradfriClient = tradfriLib.TradfriClient;
+const AccessoryTypes = tradfriLib.AccessoryTypes;
+const tradfri = new TradfriClient(process.env.TRADFRI_URL);
+
+const lightbulbs = {};
+const plugs = {};
+const devices = {};
+const groups = {};
+
+var tradfriEmitter = new events.EventEmitter();
+
+const connect = async () => {
+    try {
+        await tradfri.connect(
+            process.env.TRADFRI_USER,
+            process.env.TRADFRI_PASSWORD
+        );
+
+        tradfri.on("device updated", tradfri_deviceUpdated).observeDevices();
+
+        tradfri
+            .on("group updated", tradfri_groupUpdated)
+            .observeGroupsAndScenes();
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+function tradfri_deviceUpdated(device) {
+    devices[device.instanceId] = device;
+
+    if (device.type === AccessoryTypes.lightbulb) {
+        lightbulbs[device.instanceId] = device;
+    } else if (device.type === AccessoryTypes.plug) {
+        plugs[device.instanceId] = device;
+    }
+
+    tradfriEmitter.emit("tradfri-device-update");
+}
+
+function tradfri_groupUpdated(group) {
+    groups[group.instanceId] = group;
+    tradfriEmitter.emit("tradfri-group-update");
+}
+
+module.exports = {
+    connect: connect,
+    emitter: tradfriEmitter,
+    lightbulbs: lightbulbs,
+    plugs: plugs,
+    devices: devices,
+    groups: groups,
+};
